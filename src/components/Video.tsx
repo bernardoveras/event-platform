a9da4e967934637c5db3a3416f4c3926166d3a36
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-06-24 20:57:37 -0300

    ADicionado video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,18 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+
+import '@vime/core/themes/default.css';
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="KJj70dBgRPo" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
@@ -37,12 +45,12 @@ export function Video() {
           </div>
 
           <div className="flex flex-col gap-4">
-            <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+            <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
 
-            <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+            <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
               <Lightning size={24} />
               Acesse o desafio
             </a>
@@ -58,7 +66,7 @@ export function Video() {
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">
                 Material complementar
-                </strong>
+              </strong>
               <p className="text-sm text-gray-200 mt-2">
                 Acesse o material complementar para acelerar o seu desenvolvimento
               </p>
@@ -77,7 +85,7 @@ export function Video() {
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">
                 Walpappers exclusivos
-                </strong>
+              </strong>
               <p className="text-sm text-gray-200 mt-2">
                 Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
               </p>
